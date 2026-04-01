bd396ac02051198462c1bf30a0dc13f63b4c0ed3
m-miyawaki-m <miyawakifamilymyam@gmail.com>
2026-04-01 23:13:20 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.co.example.warehouse',
+  appName: 'ionic-sample-tmp',
+  webDir: 'dist'
+};
+
+export default config;
